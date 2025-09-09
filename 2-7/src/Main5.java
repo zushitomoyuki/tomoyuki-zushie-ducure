5b966c2fe8ebbb138db8efd75f526512a0fa1067
tomoyuki-zushi-educure <t.zushi-tunagu@kotobuki8.net>
2025-09-09 18:09:04 +0900

    Add files via upload
    Signed-off-by: tomoyuki-zushi-educure <t.zushi-tunagu@kotobuki8.net>

--- 2-7/src/Main5.java
+++ 2-7/src/Main5.java
@@ -1,12 +1,22 @@
 public class Main5 {
     public static void main(String[] args) {
-        int note=280*3;
-        int pencil=120*5;
-        int eraser=100*2;
+        int notePrice=280;
+        int pencilPrice=120;
+        int eraserPrice=100;
+
+        int notePcs=3;
+        int pencilPcs=5;
+        int eraserPcs=2;
+
+        int note=notePrice*notePcs;
+        int pencil=pencilPrice*pencilPcs;
+        int eraser=eraserPrice*eraserPcs;
+
         int subtotal=note+pencil+eraser;
         int tax=(int)(subtotal*0.1);
         int total=subtotal+tax;
         int change=2000-total;
+        
         System.out.println("小計:"+subtotal+"円");
         System.out.println("消費税:"+tax+"円");
         System.out.println("合計金額:"+total+"円");
