6432ff1ec21c96c656a13d7b46e2b22940bafd55
tomoyuki-zushi-educure <t.zushi-tunagu@kotobuki8.net>
2025-09-23 16:40:21 +0900

    Add files via upload
    Signed-off-by: tomoyuki-zushi-educure <t.zushi-tunagu@kotobuki8.net>

--- 2-12/src/Main2.java
+++ 2-12/src/Main2.java
@@ -1,10 +1,10 @@
 public class Main2 {
     public static void main(String[] args) {
-        for(int dan=1; dan<=5; dan++){
-            System.out.println(dan+"の段");
+        for(int i=1; i<=5; i++){
+            System.out.println(i+"の段");
 
-            for(int i=1; i<=9; i++){
-                System.out.println(dan+"×"+i+"="+(dan*i));
+            for(int j=1; j<=9; j++){
+                System.out.println(i+"×"+j+"="+(i*j));
             }
 
             System.out.println();
---
+++ 2-12/src/Main4.java
@@ -0,0 +1,10 @@
+public class Main4 {
+    public static void main(String[] args) {
+         int num = 10; 
+
+         do{
+            System.out.println(num);
+            num--;
+         }while(num>=0);
+    }
+}
