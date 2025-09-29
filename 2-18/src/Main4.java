bd4581fae175b37204ffc15da5df65cbed3315f7
tomoyuki-zushi-educure <t.zushi-tunagu@kotobuki8.net>
2025-09-29 21:45:56 +0900

    Add files via upload
    Signed-off-by: tomoyuki-zushi-educure <t.zushi-tunagu@kotobuki8.net>

--- 2-18/src/Main4.java
+++ 2-18/src/Main4.java
@@ -1,30 +1,21 @@
+import java.util.Arrays;
+
 public class Main4 {
     public static void main(String[] args) {
         int[] prices = {100, 200, 300};
         int[] quantities = {1, 2, 3};
+        int[] totals = calculateTotals(prices, quantities);
 
-        int[] totals = new int[prices.length];
-        for (int i = 0; i < prices.length; i++) {
-            totals[i] = (int)(prices[i] * quantities[i] * 1.08);
-        }
-
-        System.out.print("商品価格: ");
-        printArray(prices);
-        System.out.print(", 数量: ");
-        printArray(quantities);
-        System.out.println();
-        System.out.print("合計金額: ");
-        printArray(totals);
+        System.out.println("商品価格: " + Arrays.toString(prices)+ ", 数量: " + Arrays.toString(quantities));
+        System.out.println("合計金額: " + Arrays.toString(totals));
     }
 
-    public static void printArray(int[] array) {
-        System.out.print("[");
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i]);
-            if (i < array.length - 1) {
-                System.out.print(", ");
-            }
+    public static int[] calculateTotals(int[] prices, int[] quantities) {
+        int[] result = new int[prices.length];
+        for (int i = 0; i < prices.length; i++) {
+            result[i] = (int)(prices[i] * quantities[i] * 1.08);
         }
-        System.out.print("]");
+        return result;
     }
 }
+
